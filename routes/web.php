49040d8384a0f0e5a294b05e1cea072e5f456063
sergio-dotpro <sergio.ferreira@dotpro.pt>
2023-08-30 09:37:37 +0100

    teste

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::prefix('mb')->name('mb.')->group(function () {
 Route::prefix('mbway')->name('mbway.')->group(function () {
     Route::get('callback', 'MBWayController@callback')->name('callback');
 });
+
+
+Route::get('/callback', function (Request $request) {
+    dd($request);
+});
